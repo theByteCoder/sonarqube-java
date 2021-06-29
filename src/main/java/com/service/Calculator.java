510629c294d68e2ec9d63f4880aeef04996c7002
theByteCoder <47722399+theByteCoder@users.noreply.github.com>
2021-06-29 19:45:42 +0530

    feat(app): Added sonarqube gradle task

--- src/main/java/com/service/Calculator.java
+++ src/main/java/com/service/Calculator.java
@@ -29,25 +29,4 @@ class Calculator {
         if(numTwo == 0) return numTwo/ numOne;
         return numOne / numTwo;
     }
-
-}
-
-public class TestCalculator {
-
-    public static void main(String[] args) {
-
-        String exceptionMessage = "Unexpected result";
-
-        Calculator calc = new Calculator(1, 1);
-
-        if(calc.add() != 2) throw new AssertionError(exceptionMessage);
-
-        if(calc.substract() != 0) throw new AssertionError(exceptionMessage);
-
-        if(calc.multiply() != 1) throw new AssertionError(exceptionMessage);
-
-        if(calc.divide() != 1) throw new AssertionError(exceptionMessage);
-    }
-
-
 }
---
+++ src/test/java/com/service/TestCalculator.java
@@ -0,0 +1,21 @@
+package com.service;
+
+public class TestCalculator {
+
+    public static void main(String[] args) {
+
+        String exceptionMessage = "Unexpected result";
+
+        Calculator calc = new Calculator(1, 1);
+
+        if(calc.add() != 2) throw new AssertionError(exceptionMessage);
+
+        if(calc.substract() != 0) throw new AssertionError(exceptionMessage);
+
+        if(calc.multiply() != 1) throw new AssertionError(exceptionMessage);
+
+        if(calc.divide() != 1) throw new AssertionError(exceptionMessage);
+    }
+
+
+}
